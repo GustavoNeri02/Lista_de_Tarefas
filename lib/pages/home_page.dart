4764e525437dca5beab0e9fdb19d6ec1ced98696
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-04-29 00:29:43 -0300

    Primeiras implementações: *novas cores em degradê como tema, *icone de avatar (é possivel escolher foto da galeria, porém sem salvar permanentemente) *iconButton

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/widgets/icon_avatar.dart';
 import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,34 +14,65 @@ class _HomePageState extends State<HomePage> {
   List toDoList = [];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0XFF6200b3), Color(0XFF8B54C9)],
+                  stops: [0, 0.7],
+                  transform: GradientRotation(0.3)),
+            ),
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconAvatarWidget(),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
-
   //Pegar arquivo .json
-  Future<File> _getFile() async{
+  Future<File> _getFile() async {
     //pegando diretorio tanto de android como de ios
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/listaData.json");
   }
 
   //Salvar arquivo .json
-  Future<File> _saveFileData() async{
+  Future<File> _saveFileData() async {
     String data = json.encode(toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
   }
 
   //Ler arquivo .json
-  Future<String> _readData() async{
-    try{
+  Future<String> _readData() async {
+    try {
       final file = await _getFile();
       return file.readAsString();
-    }catch (e){
+    } catch (e) {
       return null;
     }
   }
-
 }
-
-
---
+++ lib/widgets/icon_avatar.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class IconAvatarWidget extends StatefulWidget {
+  @override
+  _IconAvatarWidgetState createState() => _IconAvatarWidgetState();
+}
+
+class _IconAvatarWidgetState extends State<IconAvatarWidget> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future _getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print("Nenhuma imagem selecionada");
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(00),
+            border: Border.all(color: Colors.black, width: 3)),
+        child: _image == null
+            ? Icon(
+                Icons.person_add,
+              )
+            : Image.file(
+                _image,
+                fit: BoxFit.fitWidth,
+              ),
+      ),
+      onTap: () {
+        _getImage();
+      },
+    );
+  }
+}
